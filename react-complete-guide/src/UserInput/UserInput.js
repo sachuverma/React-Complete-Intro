24075398c57fbc98efce5463d0e2df35bec4c2d6
sachinverma53121 <sachinverma53121@gmail.com>
2020-10-12 16:19:57 +0530

    list and conditionals

--- react-complete-guide/src/UserInput/UserInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserInput = (props) => {
-  return (
-    <input className="assg-input" type="text" onChange={props.nameChange} value={props.currentName}/>
-  )
-}
-
-export default UserInput;
